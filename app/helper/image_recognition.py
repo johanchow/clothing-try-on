2eaa68abffe578b72c4fcbd03d87749f70ccf08b
johanchow <johanchow@163.com>
2024-11-06 20:23:46 +0800

    更换新的分类图的CNN模型

--- app/helper/image_recognition.py
+++ app/helper/image_recognition.py
@@ -3,40 +3,41 @@ from io import BytesIO
 import numpy as np
 import tensorflow as tf
 import logging
-from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+from tensorflow import keras
 from tensorflow.keras.preprocessing import image
-from helper.resource import get_resource_from_cos
 
 # 本地开发使用，启动tensorflow时候默认下载会要求ssl
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# 加载预训练的 EfficientNetB0 模型
-model_path = './model/efficientnetb0.h5'
+# 加载预训练的Fashion Mnist CNN模型
+model_path = './app/model/fashion_mnist_model.h5'
 if os.getenv("SERVER_ENV") == 'prod':
-  model_path = './model/efficientnetb0.h5'
-model = EfficientNetB0(weights=model_path)
+  model_path = './model/mnist_fashion.h5'
+model = keras.models.load_model(model_path)
 
 def classify_image(img_bytes):
   '''
-  利用预训练的 EfficientNetB0 模型对图片(BytesIO流)进行分类
+  利用fashion mnist数据预训练的CNN模型对图片(BytesIO流)进行分类
   '''
   # 加载图片并将其调整为模型需要的输入尺寸
-  img = image.load_img(img_bytes, target_size=(224, 224))  # EfficientNetB0 的输入尺寸为 224x224
+  img = image.load_img(img_bytes, target_size=(28, 28), color_mode='grayscale')  # EfficientNetB0 的输入尺寸为 224x224
   img_array = image.img_to_array(img)  # 转换为 numpy 数组
   img_array = np.expand_dims(img_array, axis=0)  # 增加 batch 维度
-  img_array = preprocess_input(img_array)  # 预处理为模型需要的格式
+  img_array = img_array / 255.0
 
-  # 使用模型进行预测
-  predictions = model.predict(img_array)
+  # 使用模型进行预测，并取出前 2 个预测结果
+  scores = model.predict(img_array).tolist()[0]
+  print(scores)
+  top_2_indices = np.argsort(scores)[-2:]
+  [top_1_index, top_2_index] = top_2_indices[::-1]
 
-  # 将预测结果解码为可读标签
-  decoded_predictions = decode_predictions(predictions, top=5)[0]
+  return [(clothing_labels[top_1_index], scores[top_1_index]), (clothing_labels[top_2_index], scores[1])]
 
-  return decoded_predictions
-
-clothing_labels = ['dress', 'pants', 'skirt', 'coat', 'jacket']
+clothing_labels = [
+  'T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+  'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'
+]
 def is_image_clothing(img_file):
   '''
   判断图片(File流)是否是衣服
@@ -45,27 +46,10 @@ def is_image_clothing(img_file):
   img_bytes = BytesIO(img_file.read())
   logging.debug('start to classify image')
   predictions = classify_image(img_bytes)
-  most_likely1_label, most_likely1_score = predictions[0][1], predictions[0][2]
-  most_likely2_label, most_likely2_score = predictions[1][1], predictions[1][2]
+  most_likely1_label, most_likely1_score = predictions[0][0], predictions[0][1]
+  most_likely2_label, most_likely2_score = predictions[1][0], predictions[1][1]
   logging.info(f'image recognition result: {most_likely1_label}={most_likely1_score}; {most_likely2_label}={most_likely2_score}')
-  if most_likely1_label not in clothing_labels:
-     return False
-  total_score = most_likely1_score
-  if most_likely2_label in clothing_labels:
-      total_score += most_likely2_score
-  if total_score > 0.5:
+  total_score = most_likely1_score + most_likely2_score
+  if total_score > 0.2:
      return True
   return False
-
-def init_cnn_model():
-  '''
-  初始化模型，加载预训练模型
-  '''
-  global cnn_model
-  print('start to init cnn model')
-  # 加载预训练的 EfficientNetB0 模型
-  local_model_path = './efficientnetb0.h5'
-  with open(local_model_path, 'wb') as f:
-    f.write(get_resource_from_cos('libs/efficientnetb0.h5', 'binary'))
-  cnn_model = EfficientNetB0(weights=local_model_path)
-  print('finish to init cnn model')
